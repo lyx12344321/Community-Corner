8cac3eddfeb9cc14e873b913c1c6e8ac3f245387
lyx12344321 <3469686919@qq.com>
2024-10-18 14:10:49 +0800

    修改

--- src/main/java/life/mj/community/controller/AuthorizeController.java
+++ src/main/java/life/mj/community/controller/AuthorizeController.java
@@ -1,10 +1,12 @@
 package life.mj.community.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import life.mj.community.dto.AccessTokenDTO;
 import life.mj.community.dto.GitHubUser;
 import life.mj.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,7 +28,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(ClientId);
@@ -35,6 +38,12 @@ public class AuthorizeController {
         accessTokenDTO.setClient_secret(ClientSecret);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser gitHubUser = gitHubProvider.getUser(accessToken);
-        return "index";
+
+        if (gitHubUser.getId() != null) {
+            request.getSession().setAttribute("gitHubUser", gitHubUser);
+            System.out.println("登陆成功");
+        }
+
+        return "redirect:"+"/";
     }
 }
